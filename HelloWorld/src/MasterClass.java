1d3b625d5b6c8f3e2dec83e835a28953c6c44fea
roshan1 <adityaroshan053@gmail.com>
2021-07-10 16:03:09 +0530

    first commit

---
+++ HelloWorld/src/MasterClass.java
@@ -0,0 +1,12 @@
+public class MasterClass {
+
+    public static  void main (String [] args) {
+
+        int myFirstNumber=(2*4 +5 +7);
+        int mySecondNumber= 3*5;
+        int myThirdNumber=7;
+        int myTotal =myFirstNumber+mySecondNumber+myThirdNumber;
+
+        System.out.println(myTotal);
+    }
+}
